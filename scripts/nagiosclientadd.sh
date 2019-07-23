fdccb401407bc677233b447949065a4136d7ffa1
akmaljonsadikov <51516020+akmaljonsadikov@users.noreply.github.com>
2019-07-22 23:42:00 -0500

    script to add client in master nagios
    This script will be run in nagios server to add clients into the  monitor file. This is after running the nagios_client.yml on assible server.

---
+++ scripts/nagiosclientadd.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+read -p "Enter  IP number :" ip
+read -p "Enter the HOSTNAME :" hostname
+read -p "Enter hostgroupname :" hostgroupname
+read -p "Enter hostgroupname Alias :" alias
+
+cp -p /usr/local/nagios/etc/objects/localhost.cfg /usr/local/nagios/etc/servers/$hostname.cfg
+
+        sed -i "s/localhost/$hostname/g" /usr/local/nagios/etc/servers/$hostname.cfg
+        sed -i "s/"127.0.0.1"/$ip/" /usr/local/nagios/etc/servers/$hostname.cfg
+        sed -i "s/"linux-servers"/$hostgroupname/g" /usr/local/nagios/etc/servers/$hostname.cfg
+        sed -i "s/Linux Servers/$alias/g" /usr/local/nagios/etc/servers/$hostname.cfg
+
+
+systemctl restart nagios.service
